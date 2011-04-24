73f1caefd7ce37802f437b8a5b9556bf91db3ca6
Andrew Miller <amiller@dappervision.com>
2011-04-24 03:47:12 -0400

    Updated download.sh
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- download.sh
+++ download.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-wget -N https://s3.amazonaws.com/blockplayer/datasets.tar.gz
+s3cmd sync s3://blockplayer/datasets/ data/archives/
+
+for x in `ls data/archives/ | sed -e s/\.tar\.gz//`
+    pushd data/sets/$x
+    tar -xzf data/archives/$x.tar.gz
+    popd
+done
+
 #mkdir data
 tar -xzf datasets.tar.gz
