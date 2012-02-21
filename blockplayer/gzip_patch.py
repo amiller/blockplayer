cb3f9e7fb5ec77bc2d192ce85fde8dd2763e580c
Andrew Miller <amiller@dappervision.com>
2012-02-21 18:24:44 -0500

    Removed some unnecessary files. Trying out a license header in gzip_patch.py to see how it looks.
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- blockplayer/gzip_patch.py
+++ blockplayer/gzip_patch.py
@@ -1,3 +1,14 @@
+# Andrew Miller <amiller@cs.ucf.edu> (c) 2011
+# BlockPlayer - 3D model reconstruction using Lattice-First algorithm
+# See: 
+#    "Interactive 3D Model Acquisition and Tracking of Building Block Structures"
+#    Andrew Miller, Brandyn White, Emiko Charbonneau, Zach Kanzler, and Joseph J. LaViola Jr.
+#    IEEE VR 2012, IEEE TVGC 2012
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import gzip
 
 
