78bd0c4ed39685962c615ae987e00cba03e96a48
Andrew Miller <amiller@dappervision.com>
2012-02-22 02:45:26 -0500

    Modified setup.py to point to known dependencies
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules=[Extension("blockplayer.calibkinect_cy",
@@ -11,7 +11,14 @@ ext_modules=[Extension("blockplayer.calibkinect_cy",
 
 
 setup(name='BlockPlayer',
-      version='0.01',
-      packages=['blockplayer','blockplayer.visuals'],
+      version='0.1',
+      author='Andrew Miller',
+      email='amiller@cs.ucf.edu',
+      packages=['blockplayer'],
       cmdclass={'build_ext': build_ext},
-      ext_modules=ext_modules)
+      ext_modules=ext_modules,
+      install_requires=['distribute', 'cython', 'pyopencl', 'PyOpenGL', 'numpy', 'scipy'],
+      dependency_links = [
+        "https://github.com/amiller/wxpy3d/tarball/master#egg=wxpy3d-1.0",
+        "https://github.com/amiller/glxcontext/tarball/master#egg=glxcontext-1.0",
+        ])
